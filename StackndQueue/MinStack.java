5434f9dd3a3719b49effa319d6083da9f6472671
anjaneya <princetiwari97940@gmail.com>
2024-04-11 23:30:24 +0530

    next greater number I (stack nd queue)

---
+++ StackndQueue/MinStack.java
@@ -0,0 +1,36 @@
+package StackndQueue;
+
+import java.util.ArrayList;
+
+public class MinStack {
+    public static void main(String[] args){
+
+    }
+    static class minStack{
+        private ArrayList<Integer> st ;
+        private int top = -1;
+        private ArrayList<Integer> arr ;
+        private int min = Integer.MAX_VALUE;
+        public minStack(){
+            this.st = new ArrayList<>();
+            this.arr = new ArrayList<>();
+        }
+        public void push(int val){
+            top++;
+            st.add(top,val);
+            this.min = Math.min(min,val);
+            arr.add(top,min);
+        }
+        public void pop(){
+            top--;
+        }
+        public int top(){
+            return st.get(top);
+        }
+        public int getMin(){
+            return arr.get(top);
+        }
+    }
+
+}
+
